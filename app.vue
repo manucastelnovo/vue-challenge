bee9d479886cf70a76cdeaf59dc2dcd4a551952f
manucastelnovo <marnutskate@gmail.com>
2024-08-07 16:39:56 -0400

    ui

---
+++ app.vue
@@ -0,0 +1,6 @@
+<template>
+  <NuxtLayout>
+  <AppHeader/>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  pages:true,
+  modules: ["@nuxtjs/tailwindcss"],
+  components: [
+    {
+      path: '~/components',
+      pathPrefix: false, // [!code ++]
+    },
+  ],
+})
+
