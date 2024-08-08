c87afb6cba066e95bd5b64ac0c3cfb342ad1a62a
manucastelnovo <marnutskate@gmail.com>
2024-08-08 13:47:58 -0400

    add pinia plugin persisted state

--- app.vue
+++ app.vue
@@ -4,3 +4,11 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+<script setup>
+import { createPinia } from 'pinia'
+import piniaPluginPersistedState from "pinia-plugin-persistedstate"
+
+const pinia = createPinia();
+
+pinia.use(piniaPluginPersistedState)
+</script>
