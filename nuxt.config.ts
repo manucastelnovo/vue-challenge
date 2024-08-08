2a457349aeea5efe3df3881932ebfec2bc00d402
manucastelnovo <marnutskate@gmail.com>
2024-08-07 21:06:46 -0400

    store

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  pages:true,
-  modules: ["@nuxtjs/tailwindcss"],
+  pages: true,
+  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
+  pinia: {
+    storesDirs: ['./stores/**', './custom-folder/stores/**'],
+  },
   components: [
     {
-      path: '~/components',
+      path: "~/components",
       pathPrefix: false, // [!code ++]
     },
   ],
